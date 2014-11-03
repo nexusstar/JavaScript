9bee118a784cb5b41f8173c73db3e8da40097152
Petar Nikov <petar.nikov@gmail.com>
2014-11-03 18:47:04 +0200

    Set of problems

---
+++ SetOfProblems/README.md
@@ -0,0 +1,5 @@
+#JS Learning problems
+
+This is a set of problems and my try on solutions on them.
+
+Don't take it too serious
---
+++ SetOfProblems/fibonacci.js
@@ -0,0 +1,15 @@
+/**
+ * Created by p.nikov on 3.11.2014 г..
+ *
+ *  N-th Fibonacci
+ The most annoying problem of all.
+ Implement a function, called `fib(n)` that returns the n-th fibonacci number, given by the argument.
+ */
+"use strict";
+
+var fib = function(n){
+    if(n < 3){
+        return 1;
+    }
+    return fib(n-1) + fib(n-2);
+};
