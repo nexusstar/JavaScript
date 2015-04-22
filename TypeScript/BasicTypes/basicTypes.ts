b5a460670ad561be60c3b48f134dadd75ed4e906
Petar Nikov <petar.nikov@gmail.com>
2015-04-22 17:02:04 +0300

    OOP

---
+++ TypeScript/BasicTypes/basicTypes.ts
@@ -0,0 +1,20 @@
+/**
+ * Created by p.nikov on 8.4.2015 ã..
+ */
+// Number type
+
+var x: number;
+var y = 0;
+var z = 123.40;
+var s = z.toFixed(2);
+
+//Boolean type
+var b: boolean;
+var yes = true;
+var no = false;
+
+//String
+var s: string;          // Explicitly typed
+var empty = "";         // Same as empty: string = ""
+var abc = 'abc';        // Same as abc: string = "abc"
+var c = abc.charAt(2);  // Property of String interface
